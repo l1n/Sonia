b6ac41ac64f4881a062a936cd2cdf22e14bb556e
Linaea <10sdassarma@gmail.com>
2013-07-30 10:49:10 -0400

    useful errors

--- server.js
+++ server.js
@@ -438,7 +438,10 @@ function addSong() {
     upnext.push(line);
     if (settings.notify) sonia.say('#SonicRadioboom', line+' up next!');
     request('http://radio.ponyvillelive.com:2199/api.php?xm=server.playlist&f=json&a[username]=Linana&a[password]=yoloswag&a[action]=add&a[playlistname]=Temp&a[trackpath]='
-    +line, function (error, response, body) {body = JSON.parse(body); if (body.type!="success") {sonia.say('linaea', "There was an error adding '"+line+"' to the playlist. Debugging data: "+body);} })}); // TODO Change the song picker to be non-random
+    +line, function (error, response, body) {body = JSON.parse(body); if (body.type!="success") {sonia.say('linaea', "There was an error adding '"+line+"' to the playlist.");
+    sonia.say('linaea', "URL: "+'http://radio.ponyvillelive.com:2199/api.php?xm=server.playlist&f=json&a[username]=Linana&a[password]=yoloswag&a[action]=add&a[playlistname]=Temp&a[trackpath]='
+    +line);
+    sonia.say('linaea', "Body: "+JSON.stringify(body));} })}); // TODO Change the song picker to be non-random
     // request('http://radio.ponyvillelive.com:2199/api.php?xm=server.playlist&f=json&a[username]=Linana&a[password]=yoloswag&a[action]=deactivate&a[playlistname]=Temp',
     // function (error, response, body) {
         request('http://radio.ponyvillelive.com:2199/api.php?xm=server.playlist&f=json&a[username]=Linana&a[password]=yoloswag&a[action]=activate&a[playlistname]=Temp', function (error, response, body) {});
