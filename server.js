48bcd364ae238f4d5fddaef3d7bbdb400739858d
Linaea <10sdassarma@gmail.com>
2013-08-05 15:57:09 -0400

    upnext.length bug

--- server.js
+++ server.js
@@ -451,7 +451,7 @@ function updateSong() {
                     if (settings.notify) {
                         sonia.say('#SonicRadioboom', 'New Song: '+body.response.data.status.currentsong);
                     }
-                    if (upnext.length==0) upnext.unshift();
+                    if (upnext.length!=0) upnext.unshift();
                     request('http://radio.ponyvillelive.com:2199/api.php?xm=server.playlist&f=json&a[username]=Linana&a[password]=yoloswag&a[action]=remove&a[playlistname]=Temp&a[trackpath]='+
                     lastplayed.push(JSON.stringify(body.response.data.status.currentsong).trim()), function (error, response, body) {});
                     if (settings.autodj && upnext.length==0) {
